bc251f1521a795778edb90805054b65cfce4ea80
anchan828 <anchan828@gmail.com>
2019-10-24 20:33:15 +0900

    add log

--- src/save.ts
+++ src/save.ts
@@ -1,3 +1,4 @@
+import * as core from "@actions/core";
 import { dirname } from "path";
 import { existsSync, mkdirSync, writeFileSync } from "fs";
 
@@ -5,6 +6,7 @@ export async function save(text: string, file: string, encoding: string) {
   const dir = dirname(file);
   if (!existsSync(dir)) {
     mkdirSync(dir, { recursive: true });
+    core.debug(`Created directory: ${dir}`);
   }
 
   writeFileSync(file, text, encoding);
