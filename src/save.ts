0118ff16899eb4bea8cdd6b06e7794ce8f7fc273
anchan828 <anchan828@gmail.com>
2020-01-06 19:00:06 +0900

    feat: support $HOME

--- src/save.ts
+++ src/save.ts
@@ -1,9 +1,14 @@
 import * as core from "@actions/core";
-import { dirname } from "path";
+import { dirname, join } from "path";
 import { existsSync, mkdirSync, writeFileSync } from "fs";
 
 export async function save(text: string, file: string, encoding: string) {
+  if (file[0] === "~" || file[0] === "$HOME") {
+    return join(process.env.HOME!, file.slice(1));
+  }
+
   const dir = dirname(file);
+
   if (!existsSync(dir)) {
     mkdirSync(dir, { recursive: true });
     core.debug(`Created directory: ${dir}`);
